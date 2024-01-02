74ec1b2de5b5ba93a3c3aecc0ddcae9e67965011
pedro <pedrorxrxx@gmail.com>
2024-01-02 15:10:12 -0300

    feat: create Transaction entity

---
+++ src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,37 @@
+package com.picpaysimplificado.domain.transaction;
+
+import com.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne // Anotação para dizer que um usuario podem ter várias transações más uma transação so pode estar vinculada a um sender e a um receiver (Relação muitos para um).
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne // Anotação para dizer que um usuario podem ter várias transações más uma transação so pode estar vinculada a um sender e a um receiver (Relação muitos para um).
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+
+}
